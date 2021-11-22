9b478515ae2ff7dcf2e03a2043fc9d76206c9694
Bernard Van Der Vyver <Bvdv9075@gmail.com>
2021-11-22 16:09:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -58,19 +58,19 @@ def api(direction, angle):
 
 @app.route('/neo')
 def light():
-    while True:
-        t = time.time()
-        b = (math.sin(t * 2) + 1) / 2
-        b = int(b * 255.0)
-        t = round(time.time() * 1000) / 1000
-        a = round(math.sin(t) * 90)
-        pantilthat.pan(int(a))
-        pantilthat.tilt(int(a))
-        r, g, b = [int(x*255) for x in  colorsys.hsv_to_rgb(((t*100) % 360) / 360.0, 1.0, 1.0)]
-        pantilthat.set_all(r, g, b)
-        pantilthat.show()
-        print(a)
-        time.sleep(0.04)
+#     while True:
+    t = time.time()
+    b = (math.sin(t * 2) + 1) / 2
+    b = int(b * 255.0)
+    t = round(time.time() * 1000) / 1000
+    a = round(math.sin(t) * 90)
+#         pantilthat.pan(int(a))
+#         pantilthat.tilt(int(a))
+    r, g, b = [int(x*255) for x in  colorsys.hsv_to_rgb(((t*100) % 360) / 360.0, 1.0, 1.0)]
+    pantilthat.set_all(r, g, b)
+    pantilthat.show()
+#     print(a)
+#     time.sleep(0.04)
 
 if __name__ == '__main__':
 
