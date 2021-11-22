e17240bbc7b4ae60e5cb33ffd55ef73eb431eb8b
Bernard Van Der Vyver <Bvdv9075@gmail.com>
2021-11-22 16:17:34 +0000

    Update neo.py

--- neo.py
+++ neo.py
@@ -14,8 +14,8 @@ while True:
     b = int(b * 255.0)
     t = round(time.time() * 1000) / 1000
     a = round(math.sin(t) * 90)
-    pantilthat.pan(int(a))
-    pantilthat.tilt(int(a))
+#     pantilthat.pan(int(a))
+#     pantilthat.tilt(int(a))
     r, g, b = [int(x*255) for x in  colorsys.hsv_to_rgb(((t*100) % 360) / 360.0, 1.0, 1.0)]
     pantilthat.set_all(r, g, b)
     pantilthat.show()
