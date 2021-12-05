a3ad90fe33fdba5b0e7e315c5fb2a1583b80ccaa
Bernard Van Der Vyver <Bvdv9075@gmail.com>
2021-12-05 20:15:01 +0000

    Update car.py

--- car.py
+++ car.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	status_code = flask.Response(status=201)
-	return status_code
+    status_code = flask.Response(status=201)
+    return status_code
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=False)
