fb9cda43ac3d13908bc2908e2a7e2a4291513e3e
Bernard Van Der Vyver <Bvdv9075@gmail.com>
2021-12-05 20:25:33 +0000

    Update car.py

--- car.py
+++ car.py
@@ -10,12 +10,12 @@ in1 = 15
 in2 = 18
 en = 14
 
-gpio.setmode(GPIO.BCM)
-gpio.setup(in1,GPIO.OUT)
-gpio.setup(in2,GPIO.OUT)
-gpio.setup(en,GPIO.OUT)
-gpio.output(in1,GPIO.LOW)
-gpio.output(in2,GPIO.LOW)
+gpio.setmode(gpio.BCM)
+gpio.setup(in1,gpio.OUT)
+gpio.setup(in2,gpio.OUT)
+gpio.setup(en,gpio.OUT)
+gpio.output(in1,gpio.LOW)
+gpio.output(in2,gpio.LOW)
 p=gpio.PWM(en,1000)
 p.start(25)
 
